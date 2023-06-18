888e699382ab6a6e39f0c2fa7ddd2de764af724a
João Paulo <46024524+Joaopaulo987023@users.noreply.github.com>
2023-06-18 00:26:06 -0300

    finalizando projeto organo

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,15 +1,33 @@
-import {AiFillCloseCircle} from 'react-icons/ai'
+import {AiFillCloseCircle,AiFillHeart,AiOutlineHeart} from 'react-icons/ai'
 import './colaborador.css'
 
-const Colaborador = ({ colaborador, corDeFundo,aoDeletar }) => {
-    return (<div className="colaborador">
-        <AiFillCloseCircle size={25} className='deletar' onClick={aoDeletar}/>
+const Colaborador = ({ colaborador, corDeFundo,aoDeletar,aoFavoritar }) => {
+    function favoritar(){
+        aoFavoritar(colaborador.id)
+    }
+
+    const propsFavorito= {
+        size:25,
+        onClick:favoritar
+    }
+        
+    return (
+        
+        <div className="colaborador">
+        
+        <AiFillCloseCircle size={25} className='deletar' onClick={()=> aoDeletar(colaborador.id)}/>
         <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
             <img src={colaborador.imagem} alt={colaborador.nome} />
         </div>
         <div className="rodape">
             <h4>{colaborador.nome}</h4>
             <h5>{colaborador.cargo}</h5>
+            <div>{
+                colaborador.favorito 
+                    ?<AiFillHeart {...propsFavorito} color="red"/> //passando objeto como propriedade
+                    :<AiOutlineHeart {...propsFavorito}/>}
+                {/* esse objeto contem todas as propriedades que esse componente precisa */}
+            </div>
         </div>
     </div>)
 }
--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,14 +1,26 @@
+import hexToRgba from 'hex-to-rgba';
 import Colaborador from '../Colaborador'
 import './time.css'
 
-const Time = ({ time, colaboradores,aoDeletar}) => {
+const Time = ({ time, colaboradores,aoDeletar,mudaCor,aoFavoritar}) => {  
+
     return (
 
-        colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: time.corPrimaria }}>
-            <h3 style={{ borderColor: time.corSecundaria }}>{time.nome}</h3>
+        colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor:hexToRgba(time.cor, 0.6) }}>
+            <input 
+                onChange={evento=>mudaCor(evento.target.value,time.id)}
+                type="color" className='input_cor' value={time.cor}
+            />
+            <h3 style={{ borderColor: time.cor }}>{time.nome}</h3>
             <div className='colaboradores'>
                 {colaboradores.map((colaborador, indice) => {
-                    return <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.corSecundaria} aoDeletar={aoDeletar}/>}
+                    return (
+                    <Colaborador 
+                    aoFavoritar={aoFavoritar} 
+                    key={indice} colaborador={colaborador} 
+                    corDeFundo={time.cor} 
+                    aoDeletar={aoDeletar}/>
+                    )}
             )}
             </div>
         </section>
